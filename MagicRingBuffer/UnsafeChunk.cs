2146240c6809429cbac175bc6f6cd3bb327d87d8
Artem Elkin <artelkin@gmail.com>
2025-08-18 10:03:08 +0200

    exposed Chunk.Pointer, simplified Span creation

--- MagicRingBuffer/UnsafeChunk.cs
+++ MagicRingBuffer/UnsafeChunk.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace MagicRingBuffer
 {
@@ -21,20 +20,27 @@ namespace MagicRingBuffer
         public Span<T> Span
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => MemoryMarshal.CreateSpan(ref Unsafe.AsRef<T>(_ptr), _length);
+            get => new Span<T>(_ptr, _length);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Span<T> GetSpan(int start)
-            => MemoryMarshal.CreateSpan(ref Unsafe.AsRef<T>(_ptr + start), _length - start);
+            => new Span<T>(_ptr + start, _length - start);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Span<T> GetSpan(int start, int length)
-            => MemoryMarshal.CreateSpan(ref Unsafe.AsRef<T>(_ptr + start), length);
+            => new Span<T>(_ptr + start, length);
 
-        public ref T this[int index] {
+        public ref T this[int index]
+        {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => ref Unsafe.AsRef<T>(_ptr + index);
+        }
+
+        public T* Pointer
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _ptr;
         }
 
         public int Length
@@ -57,7 +63,7 @@ namespace MagicRingBuffer
         public UnsafeChunk<T> Slice(int start, int length)
              => new UnsafeChunk<T>(_ptr + start, length);
     }
-    
+
     //Memory-like struct that works a bit faster
     public unsafe readonly struct ReadOnlyUnsafeChunk<T>
         where T : unmanaged
@@ -75,20 +81,27 @@ namespace MagicRingBuffer
         public ReadOnlySpan<T> Span
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => MemoryMarshal.CreateSpan(ref Unsafe.AsRef<T>(_ptr), _length);
+            get => new ReadOnlySpan<T>(_ptr, _length);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ReadOnlySpan<T> GetSpan(int start)
-            => MemoryMarshal.CreateSpan(ref Unsafe.AsRef<T>(_ptr + start), _length - start);
+            => new ReadOnlySpan<T>(_ptr + start, _length - start);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ReadOnlySpan<T> GetSpan(int start, int length)
-            => MemoryMarshal.CreateSpan(ref Unsafe.AsRef<T>(_ptr + start), length);
+            => new ReadOnlySpan<T>(_ptr + start, length);
 
-        public ref readonly T this[int index] {
+        public ref readonly T this[int index]
+        {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => ref Unsafe.AsRef<T>(_ptr + index);
+        }
+
+        public T* Pointer
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _ptr;
         }
 
         public int Length
